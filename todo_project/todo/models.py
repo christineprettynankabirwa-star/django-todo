018b0830a9b431af8cecc77f8c178a39573a08ab
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-03-16 19:59:01 +0300

    Creating a To-do Model

--- todo_project/todo/models.py
+++ todo_project/todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    title = models.Charfield(max_length=200)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+# this model here stores the task title and the completed status
