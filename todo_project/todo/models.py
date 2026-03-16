9e842fe323cf8d50b8201811bdde942bf7f0cbe0
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-03-16 20:03:19 +0300

    debugged title (charfield to CharField)

--- todo_project/todo/models.py
+++ todo_project/todo/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Task(models.Model):
-    title = models.Charfield(max_length=200)
+    title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
 
     def __str__(self):
