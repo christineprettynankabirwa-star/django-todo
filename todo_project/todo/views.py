588e9a5596de27388b862d4caac8a6eab11a72dc
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-03-18 18:47:37 +0300

    created a View

--- todo_project/todo/views.py
+++ todo_project/todo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
 # Create your views here.
+from .models import Task
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'todo/tasks.html', {'tasks': tasks})
+
+
