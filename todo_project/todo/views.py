4ce405ecf14fad67a7e04c37ae72ac07b62243bd
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-03-22 23:48:54 +0300

    if the user submits form then save task then reload the page otherwise just display tasks

--- todo_project/todo/views.py
+++ todo_project/todo/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 # Create your views here.
 from .models import Task
 
 def task_list(request):
+    if request.method == "POST":
+        title = request.POST.get('title') 
+        if title:
+            Task.objects.create(title=title) 
+
+        return redirect('task_list')
+    
     tasks = Task.objects.all()
     return render(request, 'todo/tasks.html', {'tasks': tasks})
 
