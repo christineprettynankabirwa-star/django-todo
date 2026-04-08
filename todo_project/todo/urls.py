a62d02079fffbc7c4b24b6a9dac89201027139d2
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-04-08 15:10:34 +0300

    updated

--- todo_project/todo/urls.py
+++ todo_project/todo/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from .views import task_list, TasklistCreate, TaskUpdateDelete 
 
 urlpatterns = [
+    #HTML page
     path('', task_list, name='task_list'),
 
-    #API endpoints
-    path('api/tasks/', TaskListCreate.as_view())
-    path('api/tasks/<int:pk>/', TaskUpdateDelete.as_view()),
+#API endpoints(DRF)
+    path('api/tasks/', TaskListCreate.as_view() name='api_tasks'),
+    path('api/tasks/<int:pk>/', TaskUpdateDelete.as_view() name='api_task_detail'),
 ] 
