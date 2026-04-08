cee599ad33163fae957ddd7d6054fbe55ce9fa92
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-04-08 15:14:39 +0300

    typed a different letter in a word TaskList with small 'l' at first

--- todo_project/todo/urls.py
+++ todo_project/todo/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import task_list, TasklistCreate, TaskUpdateDelete 
+from .views import task_list, TaskListCreate, TaskUpdateDelete 
 
 urlpatterns = [
     #HTML page
     path('', task_list, name='task_list'),
 
 #API endpoints(DRF)
-    path('api/tasks/', TaskListCreate.as_view() name='api_tasks'),
-    path('api/tasks/<int:pk>/', TaskUpdateDelete.as_view() name='api_task_detail'),
+    path('api/tasks/', TaskListCreate.as_view(), name='api_tasks'),
+    path('api/tasks/<int:pk>/', TaskUpdateDelete.as_view(), name='api_task_detail'),
 ] 
